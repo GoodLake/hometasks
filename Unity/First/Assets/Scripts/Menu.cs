88539ba15b5981a675f557466b582d24b3a47180
GoodLake <vovachiryatiev@gmail.com>
2018-05-03 23:24:57 +0700

    First project init

---
+++ Unity/First/Assets/Scripts/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    public int window;
+
+    void Start()
+    {
+        window = 1;
+    }
+    private void OnGUI()
+    {
+        GUI.BeginGroup(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200));
+        if (window == 1)
+        {
+            if (GUI.Button(new Rect(10, 30, 180, 30), "Играть"))
+            {
+                window = 2;
+            }
+            if (GUI.Button(new Rect(10, 150, 180, 30), "Выход"))
+            {
+                window = 3;
+            }
+        }
+
+        if (window == 2)
+        {
+            Application.LoadLevel(1);
+        }
+        if (window == 5)
+        {
+            GUI.Label(new Rect(50, 10, 180, 30), "Вы уже выходите?");
+            if (GUI.Button(new Rect(10, 40, 180, 30), "Да"))
+            {
+                Application.Quit();
+            }
+            if (GUI.Button(new Rect(10, 80, 180, 30), "Нет"))
+            {
+                window = 1;
+            }
+        }
+        GUI.EndGroup();
+    }
+}
---
+++ Unity/First/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject MyGameObject;
+    private bool isFloor;
+    public Rigidbody2D bullet;
+
+
+    private void FixedUpdate()
+    {
+
+    }
+    private void Start()
+    {
+        isFloor = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * speed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && isFloor)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 300);
+            isFloor = false;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Rigidbody2D BulletPrefab = (Rigidbody2D)Instantiate(bullet, transform.position, transform.rotation);
+            BulletPrefab.velocity = transform.forward * 10;
+            BulletPrefab.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            Destroy(BulletPrefab, 1f);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "Platform")
+        {
+            isFloor = true;
+        }
+    }
+}
